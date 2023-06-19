223376b0b9473ecff937f8fc9efbc43c1e362571
wassimbnh <104159727+wassimbnh@users.noreply.github.com>
2023-06-19 16:36:43 +0100

    upload to firabase

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import { storage } from './firebase';
+import { ref, uploadBytes, listAll } from 'firebase/storage'
+import { v4 } from 'uuid'
 
 function App() {
 
+const [imageUpload, setImageUpload] = useState(null);
+const [imageList, setImageList] = useState([]);
+const imageListRef = ref(storage, "images/");
 
   const uploadImage = () =>{
 
-  }
+    if(imageUpload == null) 
+      return;
+ 
+    const imageRef = ref(storage,`images/${imageUpload.name + v4()}`)
+    uploadBytes(imageRef, imageUpload).then(() =>{
+      alert("Image uploaded")
+    })
+    
+  };
 
+  useEffect(() =>{
+    listAll(imageListRef).then((response) =>{
+      console.log(response)
+    })
+  },[]);
 
   return (
     <div className="App">
-      <input type="file"></input>
+      <input type="file" onChange={(event) => setImageUpload(event.target.files[0])}/>
       <button onClick={uploadImage}>Upload Image</button>
     </div>
   );
